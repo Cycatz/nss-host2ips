848b102babdae9eda8099ecae9b015a14e1b1cbd
Cycatz <thesummernightcat@gmail.com>
2020-11-25 17:21:03 +0800

    Change the return value of functions 0 represents failed 1 represents success

--- src/config_parser.c
+++ src/config_parser.c
@@ -1,11 +1,12 @@
 #include "config_parser.h"
 
-int is_valid_line(char *s) 
+int is_valid_line(char *s)
 {
     char *l = s;
 
-    if (l && *l == '#') return 1;
-    while(*l != '\n') {
+    if (!l || *l == '#')
+        return 1;
+    while (*l != '\n') {
         if (!isspace(*l++)) {
             return 0;
         }
@@ -13,14 +14,14 @@ int is_valid_line(char *s)
     return 1;
 }
 
-int initialize_host_list(HOST_LIST **host_list) 
+int initialize_host_list(HOST_LIST **host_list)
 {
-    my_malloc_exit(*host_list, sizeof(HOST_LIST));
-    
+    my_malloc(*host_list, sizeof(HOST_LIST), 0);
+
     (*host_list)->host_head = NULL;
     (*host_list)->host_tail = NULL;
 
-    return 0;
+    return 1;
 }
 
 int free_host_list(HOST_LIST *host_list)
@@ -29,7 +30,7 @@ int free_host_list(HOST_LIST *host_list)
     HOST_INFO *host_info, *info_next;
 
     host = host_list->host_head;
-    
+
     while (host) {
         host_info = host->info_head;
         host_next = host->host_next;
@@ -43,17 +44,17 @@ int free_host_list(HOST_LIST *host_list)
             host_info = info_next;
         }
 
-        host = host_next; 
+        host = host_next;
     }
     free(host_list);
-    
-    return 0;
+
+    return 1;
 }
 
 int add_new_host(HOST_LIST *host_list, HOST *host)
 {
     if (host == NULL) {
-        return 1;
+        return 0;
     }
     if (!(host_list->host_tail)) {
         host_list->host_head = host;
@@ -62,67 +63,66 @@ int add_new_host(HOST_LIST *host_list, HOST *host)
     }
     host_list->host_tail = host;
 
-    return 0;
+    return 1;
 }
 
 int add_new_host_info(HOST *host, HOST_INFO *host_info)
 {
     if (host_info == NULL) {
-        return 1;
+        return 0;
     }
-    if(!(host->info_tail)) {
+    if (!(host->info_tail)) {
         host->info_head = host_info;
     } else {
         host->info_tail->info_next = host_info;
     }
     host->info_tail = host_info;
 
-    return 0;
+    return 1;
 }
 
 
-int parse_host_name(char *line, HOST *host) 
+int parse_host_name(char *line, HOST *host)
 {
-    my_malloc(host->name, strlen(line) + 1 - 5, 1); 
+    my_malloc(host->name, strlen(line) + 1 - 5, 0);
     strncpy(host->name, line + 5, strlen(line) + 1 - 5);
-    return 0;
+    return 1;
 }
 
 int parse_host_info(char *info, HOST_INFO *host_info)
 {
-
     int s;
     char *addr_str, *if_name, *if_addr_str;
 
     addr_str = strtok(info, " \t");
     s = inet_pton(AF_INET, addr_str, &(host_info->addr));
-    
+
     if (s <= 0) {
         if (s == 0) {
             fprintf(stderr, "Not in presentation format");
         } else {
             perror("inet_pton");
         }
-        return 1;
+        return 0;
     }
-    
+
     if_name = strtok(NULL, " \t");
-    my_malloc(host_info->if_name, strlen(if_name) + 1, 1);
+    my_malloc(host_info->if_name, strlen(if_name) + 1, 0);
     strncpy(host_info->if_name, if_name, strlen(if_name));
 
     if_addr_str = strtok(NULL, " \t");
     s = inet_pton(AF_INET, if_addr_str, &(host_info->if_addr));
-    
+
     if (s <= 0) {
         if (s == 0) {
             fprintf(stderr, "Not in presentation format");
         } else {
             perror("inet_pton");
         }
-        return 1;
+        return 0;
     }
 
-    return 0;
+    return 1;
 }
 
 
@@ -132,31 +132,34 @@ int parse_config_file(const char *path, HOST_LIST *host_list)
     HOST *host = NULL;
     HOST_INFO *host_info = NULL;
 
-    char line[MAX_LINE_SIZE]; 
+    char line[MAX_LINE_SIZE];
 
     fp = fopen(path, "r");
     if (fp == NULL) {
         perror("fopen");
-        exit(EXIT_FAILURE);
+        return 0;
     }
-    
-    while(fgets(line, MAX_LINE_SIZE - 1, fp)) {
+
+    while (fgets(line, MAX_LINE_SIZE - 1, fp)) {
         if (!is_valid_line(line)) {
             line[strlen(line) - 1] = '\0';
             if (strncmp(line, "host", 4) == 0) {
                 add_new_host(host_list, host);
-                my_malloc(host, sizeof(HOST), 1);
-                parse_host_name(line,  host);
+                my_malloc(host, sizeof(HOST), 0);
+                parse_host_name(line, host);
             } else {
-                my_malloc(host_info, sizeof(HOST_INFO), 1);
+                my_malloc(host_info, sizeof(HOST_INFO), 0);
                 parse_host_info(line, host_info);
                 add_new_host_info(host, host_info);
             }
         }
     }
-    add_new_host(host_list, host);
 
-    return 0;
+    if (!add_new_host(host_list, host)) {
+        return 0;
+    }
+
+    return 1;
 }
 
 int print_host_list(HOST_LIST *host_list)
@@ -164,27 +167,28 @@ int print_host_list(HOST_LIST *host_list)
     HOST *host = host_list->host_head;
     HOST_INFO *host_info;
 
-    while(host) {
+    while (host) {
         printf("Host name: %s\n", host->name);
         printf("Fallback lists:\n");
         printf("=========================\n");
-        host_info = host->info_head;    
+        host_info = host->info_head;
         while (host_info) {
-            printf("Fallback IP address: %s\n",  inet_ntoa(host_info->addr));
-            printf("Adopt it when the IP address of interface %s is %s\n", host_info->if_name, inet_ntoa(host_info->if_addr));
+            printf("Fallback IP address: %s\n", inet_ntoa(host_info->addr));
+            printf("Adopt it when the IP address of interface %s is %s\n",
+                   host_info->if_name, inet_ntoa(host_info->if_addr));
             host_info = host_info->info_next;
         }
-        host = host->host_next; 
+        host = host->host_next;
     }
 
-    return 0;
+    return 1;
 }
 
-void parsing_test() 
+void parsing_test()
 {
     HOST_LIST *host_list;
     initialize_host_list(&host_list);
-    parse_config_file("./example1.syntax", host_list); 
+    parse_config_file("./example1.syntax", host_list);
     print_host_list(host_list);
     free_host_list(host_list);
 }
--- src/config_parser.h
+++ src/config_parser.h
@@ -1,44 +1,34 @@
 #ifndef CONFIG_PARSER_H
 #define CONFIG_PARSER_H
 
-#include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 #include <arpa/inet.h>
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define my_malloc(x, size, ret)     \
-    do {                            \
-        (x) = malloc((size));       \
-        if ((x) == NULL) {          \
-            perror("malloc");       \
-            return (ret);           \
-        }                           \
+#define my_malloc(x, size, ret) \
+    do {                        \
+        (x) = malloc((size));   \
+        if ((x) == NULL) {      \
+            perror("malloc");   \
+            return (ret);       \
+        }                       \
     } while (0)
-
-#define my_malloc_exit(x, size)     \
-    do {                            \
-        (x) = malloc((size));       \
-        if ((x) == NULL) {          \
-            perror("malloc");       \
-            exit(EXIT_FAILURE);     \
-        }                           \
-    } while (0)
-
 
 #define MAX_LINE_SIZE 128
 
 typedef struct HOST_INFO_ELE {
-    char                 *if_name;
-    struct in_addr        if_addr;  
-    struct in_addr        addr;  
-    struct HOST_INFO_ELE *info_next; 
-} HOST_INFO ;
+    char *if_name;
+    struct in_addr if_addr;
+    struct in_addr addr;
+    struct HOST_INFO_ELE *info_next;
+} HOST_INFO;
 
 typedef struct HOST_ELE {
-    char      *name;
-    HOST_INFO *info_head, *info_tail;    
+    char *name;
+    HOST_INFO *info_head, *info_tail;
     struct HOST_ELE *host_next;
 } HOST;
 
@@ -48,12 +38,12 @@ typedef struct {
 
 int is_valid_line(char *);
 
-int initialize_host_list(HOST_LIST **); 
-int free_host_list(HOST_LIST *); 
+int initialize_host_list(HOST_LIST **);
+int free_host_list(HOST_LIST *);
 int print_host_list(HOST_LIST *);
 
-int add_new_host(HOST_LIST *, HOST *); 
-int add_new_host_info(HOST *, HOST_INFO *); 
+int add_new_host(HOST_LIST *, HOST *);
+int add_new_host_info(HOST *, HOST_INFO *);
 
 int parse_config_file(const char *, HOST_LIST *);
 int parse_host_name(char *, HOST *);
