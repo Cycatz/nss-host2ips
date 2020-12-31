4c7967348a1b579b5b0d6f9bb44a44db3a5c1f08
Cycatz <thesummernightcat@gmail.com>
2020-12-31 15:06:58 +0800

    Add rough test script

---
+++ test/test.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+RESET="\033[22;49;39m"
+RED="\033[0;31m"
+GREEN="\033[1;32m"
+BLUE="\033[1;34m"
+
+TEST_BINARY="../target/run_test"
+CONFIG_FILE=${1:-"/etc/host2ips"}
+hosts_list=()   
+tests_list=(
+	# parse_test
+	library_test
+	# ssh_test
+	# curl_test
+	getent_test
+	ping_test
+)
+
+
+parse_test(){
+	echo -e "${BLUE}Running parse test...${RESET}"
+	$TEST_BINARY parse "$CONFIG_FILE"
+}
+library_test(){
+	echo -e "${BLUE}Running library test...${RESET}"
+	for host in "${hosts_list[@]}"; do
+		echo "$host: "
+		"$TEST_BINARY" library "$host"
+		local ret="$?"
+		if [[ $ret -ne 0 ]]; then
+			ret_val="$ret"
+			break; 
+		fi
+	done
+	ret_val=0
+}
+ssh_test() {
+	:
+}
+curl_test() {
+	:
+}
+getent_test() {
+	echo -e "${BLUE}Running getent test...${RESET}"
+	for host in "${hosts_list[@]}"; do
+		getent hosts "$host"
+		local ret="$?"
+		if [[ $ret -ne 0 ]]; then
+			ret_val="$ret"
+			break; 
+		fi
+	done
+	ret_val=0
+}
+ping_test() {
+	echo -e "${BLUE}Running ping test...${RESET}"
+	for host in "${hosts_list[@]}"; do
+		ping -c 1 "$host" | head -n 1 | awk '{print $2 $3}'
+		local ret="$?"
+		if [[ $ret -ne 0 ]]; then
+			ret_val="$ret"
+			break; 
+		fi
+	done
+	ret_val=0
+}
+
+main() {
+	readarray -t hosts_list < <(cat "$CONFIG_FILE" | grep -v '^#' | grep host | awk '{print $2}')
+	for func in "${tests_list[@]}"; do
+		$func
+	    if [[ $ret_val -eq 0 ]]; then
+        	echo -e "${GREEN}Success!${RESET}"
+			echo ""
+        else
+        	echo -e "${RED}Failed!${RESET}"
+        	exit 1
+	    fi
+    done
+    exit 0
+}
+
+main "$@"
